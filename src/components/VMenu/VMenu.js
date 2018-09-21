c37e6a4bf2dd8dd2b44e2d3658df244ced1010b5
Jacek Karczmarczyk <jk@drukcd.pl>
2018-09-21 08:48:22 +0200

    feat: v-menu keyable prop
    fixes #5103

--- src/components/VMenu/VMenu.js
+++ src/components/VMenu/VMenu.js
@@ -198,9 +198,9 @@ export default Vue.extend({
         name: 'resize',
         value: this.onResize
       }],
-      on: {
+      on: this.keyable ? {
         keydown: this.onKeyDown
-      }
+      } : undefined
     }
 
     return h('div', data, [
